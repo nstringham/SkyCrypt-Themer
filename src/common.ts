913dbfb654c172818740522db8757bd940770d96
Nate Stringham <nstringham@wccnet.edu>
2020-12-20 01:40:03 -0500

    fix: update type declaration of Theme

--- src/common.ts
+++ src/common.ts
@@ -2,24 +2,28 @@ export interface Theme {
     name: string,
     author: string,
     official: boolean,
-    logo: "../img/logo_square.svg",
+    logo: ImageUrl,
     backgrounds?: {
         bg?: {
-            png?: "../img/bg.png?v3",
-            webp?: "../img/bg.webp?v3"
+            png?: ImageUrl,
+            webp?: ImageUrl
         },
         bg_blur?: {
-            webp?: "../img/bg_blur.webp?v3"
+            webp?: ImageUrl
         }
     },
     colors?: {
-        icon?: "#0BCA51",
-        line?: "#0BDA51",
-        link?: "#0BEA51",
-        hover?: "#09EF70",
-        maxed?: "#DD980E",
-        gold?: "#FDBB3C",
-        skillbar?: "#0BAA51",
-        maxedbar?: "#CE8F12"
+        icon?: Color,
+        line?: Color,
+        link?: Color,
+        hover?: Color,
+        maxed?: Color,
+        gold?: Color,
+        skillbar?: Color,
+        maxedbar?: Color
     }
 }
+
+type Color = string
+
+type ImageUrl = string
