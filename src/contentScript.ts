a4d10efb35a43dcab6a822fb4664727ecf72b491
Nate Stringham <nstringham@wccnet.edu>
2020-12-20 06:37:52 -0500

    refactor: make injected script a module

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,4 +1,5 @@
 const scriptElement = document.createElement('script');
+scriptElement.type = "module";
 scriptElement.src = chrome.runtime.getURL('injectable.js');
 document.head.appendChild(scriptElement);
 
