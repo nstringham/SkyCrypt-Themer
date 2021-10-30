8e62bc0fbd249b7f66d5c9d1f346ab1e39dbc014
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2021-10-30 19:54:26 -0400

    build: :recycle: rollup

---
+++ rollup.config.ts
@@ -0,0 +1,30 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+
+import { chromeExtension, simpleReloader } from "rollup-plugin-chrome-extension";
+import { emptyDir } from "rollup-plugin-empty-dir";
+import zip from "rollup-plugin-zip";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+export default {
+  input: "src/manifest.json",
+  output: {
+    dir: "dist",
+    format: "esm",
+    sourcemap: true,
+  },
+  plugins: [
+    chromeExtension(),
+    simpleReloader(),
+    resolve(),
+    commonjs(),
+    typescript(),
+    postcss(),
+    emptyDir(),
+    isProduction && zip({ dir: "releases" }),
+  ],
+};
