ffc5a0981a4349c3168e3be86797c591ebc4b569
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2021-12-20 04:21:35 -0500

    refactor: :fire: remove injectable script

--- src/contentScriptStart.ts
+++
@@ -1,4 +0,0 @@
-const scriptElement = document.createElement("script");
-scriptElement.type = "module";
-scriptElement.src = chrome.runtime.getURL("injectable.js");
-document.documentElement.prepend(scriptElement);
--- src/injectable.ts
+++
@@ -1,1 +0,0 @@
-console.log("SkyCrypt Themer successfully injected");
